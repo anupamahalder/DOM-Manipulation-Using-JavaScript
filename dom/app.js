7edcd69a28e3c8a7e87e7d40a53c779156c704f0
Anupama_Halder <anupamahalder2022@gmail.com>
2024-06-08 21:02:51 +0530

    Apply loop and if-else to perform operations

--- dom/app.js
+++ dom/app.js
@@ -13,3 +13,20 @@ const target2 = document.getElementById("title");
 console.log(target2); // getting the element directly
 
 target2.style.color = "blue";
+
+// To select all gtml elements at a time by passing tag name/classname 
+console.log(document.querySelectorAll("h1")); // getting an array
+console.log(document.querySelectorAll(".title")); // getting an array
+
+const allBox = document.getElementsByClassName("box");
+console.log(allBox);
+// loop through the allBox array 
+for(let i=0;i<allBox.length;i++){
+    const element = allBox[i];
+
+    // make 5th box in different color 
+    if(element.innerText == "Box 5"){
+        element.style.backgroundColor = "orange";
+    }
+    else element.style.backgroundColor = "lightgreen";
+}
