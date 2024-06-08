090db7eb644ac0200d7658e832b8aeea88ae5d41
Anupama_Halder <anupamahalder2022@gmail.com>
2024-06-08 20:21:20 +0530

    Understanding the selectors to select html elements

--- dom/app.js
+++ dom/app.js
@@ -1,3 +1,15 @@
-// alert("Hi, what's up?");
-
 console.log("This is my dom project");
+
+
+// ----------------Using selectors to select html elements------------ //
+console.log(document.getElementsByTagName('h1')); // getting an array
+
+// selecting a tag by class name 
+const target =  document.getElementsByClassName("title");
+console.log(target); // getting an array 
+
+// To not get array we can select by id 
+const target2 = document.getElementById("title");
+console.log(target2); // getting the element directly
+
+target2.style.color = "blue";
